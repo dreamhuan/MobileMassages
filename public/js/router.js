a4d8b1647879b89826e356c49f4cf4ee37ec750f
dreamhuan <fu_kaiqi@qq.com>
2017-06-22 11:31:49 +0800

    booking的选项以及二级路由跳转

--- public/js/router.js
+++ public/js/router.js
@@ -3,17 +3,81 @@ angular.module('app.routes', ['ui.router'])
         $urlRouterProvider.otherwise('/home');
 
         $stateProvider
-            .state('home', {
-                url: '/home',
-                templateUrl: './templates/Home.html',
-                controller: 'homeCtrl',
-            })
             .state('test', {
                 url: '/test',
                 templateUrl: './templates/test.html',
                 controller: 'testCtrl',
             })
 
+            .state('home', {
+                url: '/home',
+                templateUrl: './templates/Home.html',
+                controller: 'homeCtrl',
+            })
+
+            .state('booking', {
+                url: '/booking',
+                abstract: true,
+                templateUrl: './templates/BookIng.html',
+                controller: 'bookingCtrl',
+            })
+
+            .state('booking.step1', {
+                url: '/step1',
+                templateUrl: './templates/Step1.html',
+                controller: 'step1Ctrl',
+            })
+            .state('booking.step2', {
+                url: '/step2',
+                templateUrl: './templates/Step2.html',
+                controller: 'step2Ctrl',
+            })
+            .state('booking.step3', {
+                url: '/step3',
+                templateUrl: './templates/Step3.html',
+                controller: 'step3Ctrl',
+            })
+            .state('booking.step4', {
+                url: '/step4',
+                templateUrl: './templates/Step4.html',
+                controller: 'step4Ctrl',
+            })
+
+            .state('therapist', {
+                url: '/therapist',
+                templateUrl: './templates/Therapist.html',
+                controller: 'therapistCtrl',
+            })
+
+            .state('styles', {
+                url: '/styles',
+                templateUrl: './templates/Styles.html',
+                controller: 'stylesCtrl',
+            })
+
+            .state('pricing', {
+                url: '/pricing',
+                templateUrl: './templates/Pricing.html',
+                controller: 'pricingCtrl',
+            })
+
+            .state('faq', {
+                url: '/faq',
+                templateUrl: './templates/FAQ.html',
+                controller: 'faqCtrl',
+            })
+
+            .state('contactus', {
+                url: '/contactus',
+                templateUrl: './templates/ContactUs.html',
+                controller: 'contactusCtrl',
+            })
+
+            .state('signin', {
+                url: '/signin',
+                templateUrl: './templates/SignIn.html',
+                controller: 'signinCtrl',
+            })
 
         ;
     });
