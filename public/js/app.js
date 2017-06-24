81626819ffb75754dfb62f7e8648a6e5e7f2b44d
dreamhuan <fu_kaiqi@qq.com>
2017-06-24 12:31:35 +0800

    修改navbar样式 修改booking的nav样式，完成step2前半部分以及数据填充

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ angular.module('app', [
 
     .filter("myDateTime", function () { //ISODate格式到本地时间转换
         return function (input) {
-            return  new Date(input).Format("yyyy-MM-dd hh:mm:ss");
+            return new Date(input).Format("yyyy-MM-dd hh:mm:ss");
         }
     })
 
@@ -18,6 +18,7 @@ angular.module('app', [
         $rootScope.$on('$routeChangeStart', function (evt, next, current) {
 
         });
+
         // 对Date的扩展，将 Date 转化为指定格式的String
         // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
         // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
@@ -39,5 +40,17 @@ angular.module('app', [
                     fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ?
                         (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
             return fmt;
-        }
+        };
+
+        //根据url获取当前的路由名
+        //要求：路由设计时的url和state名要完全一致
+        //使用：document.getCurrentState();
+        //注意不要放在页面刚切换时使用，因为切换路由后url的改变会有延迟，所以放在刚加载时调用就会得到上个state
+        //或者这么用$timeout(function(){document.getCurrentState()},0);//加到任务队列尾部，确保渲染完后再调用
+        Document.prototype.getCurrentState = function () {
+            let url = document.URL;
+            let statePos = url.indexOf('/#!/') + 4;//起点是"/#!/"的最后那个/后面的内容
+            let state = url.substring(statePos, url.length);
+            return state.replace('/', '.');
+        };
     });
