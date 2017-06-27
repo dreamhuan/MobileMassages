6dde7cda14aaad85ebee0d4cdbc715a5c980e88f
lvjingzhi <625679084@qq.com>
2017-06-27 10:31:48 +0800

    导入数据

---
+++ public/data/SELECT_t___FROM_mobilemassages_user_t.sql
@@ -0,0 +1,10 @@
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (1, 'Xu', 'Zero', '87987987@qq.com', 15398755641, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (2, 'Zhou', 'Meilin', '98662952@gmail.com', 15568749635, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (3, 'Kenway', 'Connor', '175644190@qq.com', 15381555478, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (4, 'Er', 'Gouzi', '29841518522@qq.com', 15289635874, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (5, 'Shi', 'Lezhi', '8513541111@qq.com', 15155568886, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (6, 'Doma', 'Umaru', '6523443221@163.com', 15388882222, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (7, 'Hoppou', 'Seiki', '65464616@qq.com', 15342656212, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (8, 'Mercer', 'Alex', 'aleixy@yahoo.com', 15388652247, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (9, 'Wang', 'Ada', '885262137@qq.com', 15322356543, 123);
+INSERT INTO mobilemassages.user (id, firstName, lastName, emaillAddress, mobileNumber, passWord) VALUES (10, 'Chu', 'Xuan', '1326578884@qq.com', 15315832556, 123);
